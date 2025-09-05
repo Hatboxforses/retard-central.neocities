dc2e0f04830c8176dcb28b24e7f48089308ff572
Hatboxforses <116845673+Hatboxforses@users.noreply.github.com>
2025-09-05 02:47:45 -0400

    Big Change in Everything
    * Re-added webamp in a different way (the last way was garbage)
    * Added scaling on mobile (<320 screens will be redirected to mobilemini.html)
    * Fixed scaling issues (using box-scaling in css should fix most scaling problems)

--- resolution.js
+++ resolution.js
@@ -1,4 +1,4 @@
-	if (window.innerWidth < 1650) {
+	if (window.innerWidth < 1670) {
   window.location.href = "small.html"; 
 } else {
   window.location.href = "main.html"; 
---
+++ resolutionm.js
@@ -0,0 +1,5 @@
+	if (window.innerWidth < 360) {
+  window.location.href = "mobilemini.html"; 
+} else {
+  window.location.href = "mobile.html"; 
+}
